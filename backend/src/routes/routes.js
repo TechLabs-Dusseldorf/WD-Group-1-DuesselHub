92e0c5c3ba1b1e18b3ab5f9040623ed586b6d465
SohailJannessari <sjanome@gmail.com>
2026-02-15 22:06:28 +0100

    cloudinary added for photos+some validations

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -1,32 +1,34 @@
 import dotenv from "dotenv"
 import express from "express"
 import Issue from "../models/Issue.js"
+import upload from "../middleware/upload.js";
+import cloudinary from "../config/cloudinary.js";
+
 
 dotenv.config()
 
+
 const router = express.Router()
 
-router.post('/issues', async (req, res) => {
+router.post('/issues', upload.single('photo'), async (req, res) => {
   try {
-    const {
-      title,
-      name,
-      description,
-      location,
-      photoUrl
-    } = req.body
-    if (!title) {
-        return res.status(400).json({ message: "Please enter a title." })
-    }
-    /*check for the conditions of name, description, location, and photo format and size*/
-    if(!name) {
-        return res.status(400).json({ message: "Please enter your name."})
-    }
-    if(!description) {
-        return res.status(400).json({ message: "Please write your report."})
-    }
-    if(!location) {
-        return res.status(400).json({ message: "Please write where the issue is located."})
+    const { title, name, description, location } = req.body;
+
+    let photoUrl = null;
+
+    if (req.file) {
+      const result = await new Promise((resolve, reject) => {
+        const stream = cloudinary.uploader.upload_stream(
+          { folder: "issues" },
+          (error, result) => {
+            if (error) reject(error);
+            else resolve(result);
+          }
+        );
+        stream.end(req.file.buffer);
+      });
+
+      photoUrl = result.secure_url;
     }
 
     const issue = new Issue({
@@ -35,17 +37,21 @@ router.post('/issues', async (req, res) => {
       description,
       location,
       photoUrl
-    })
+    });
 
-    const savedIssue = await issue.save()
+    const savedIssue = await issue.save();
 
-    res.status(201).json(savedIssue)
+    res.status(201).json(savedIssue);
+
   } catch (error) {
-    /*handle this in controller?*/
-    res.status(500).json({ message: "The report cannot be submitted. Please try again." })
-  }
-})
 
+    if (error.name === "ValidationError") {
+      return res.status(400).json({ message: error.message });
+    }
+
+    res.status(500).json({ message: "The report cannot be submitted. Please try again." });
+  }
+});
 
 
 router.get('/test', (req, res) => {
@@ -57,10 +63,11 @@ router.get('/issues', async (req, res) => {
         const issues = await Issue.find({})
         
         res.status(200).json(issues)
-    } /*handle this in controller*/
+    } 
     catch (error) {
         res.status(500).json({ message: error.message })
     }
 })
+
 
 export default router
