364e58fec222d0ca2cf21a9a939b311f54d63381
TNxRebel <housemfarhani@gmail.com>
2026-03-21 23:57:57 +0100

    Refactor 'Hottest' sorting logic

--- backend/src/controllers/commentController.js
+++ backend/src/controllers/commentController.js
@@ -1,4 +1,4 @@
-import Comment from "../models/Comment.js";
+import Comment from "../models/comment.js";
 import Issue from "../models/Issue.js";
 import mongoose from "mongoose";
 import { validateComment } from "../middleware/validateComments.js";
